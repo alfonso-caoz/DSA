e3536cd8d6dd9056c3febc4e51569a71ed2296b6
Alfonso Castillo Orozco <38094003+alfonso441@users.noreply.github.com>
2025-11-01 22:40:09 -0600

    Time: 12 ms (38.15%) | Memory: 11.9 MB (22.31%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Floyd´s cycle finding algorithm or Hare and Tortoise Algorithm
+    // Time complexity O(n). Space complexity O(1).
+    bool hasCycle(ListNode* head) {
+
+        ListNode* tortoise = head;
+        ListNode* hare = head;
+
+        while (hare && hare->next) {
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+            if (tortoise == hare) return true;
+        }
+
+        return false;
+
+    }
+};
