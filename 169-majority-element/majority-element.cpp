a13a514c42cf29ae9a157192b6c059264a350688
Alfonso Castillo Orozco <38094003+alfonso441@users.noreply.github.com>
2025-11-02 18:59:08 -0600

    Time: 0 ms (100.00%) | Memory: 27.8 MB (99.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -6,9 +6,9 @@ public:
         int candidate = 0;
         int votes = 0;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (votes == 0) candidate = nums[i]; 
-            votes += (nums[i] == candidate) ? 1 : -1;
+        for (const auto& n : nums) {
+            if (votes == 0) candidate = n; 
+            votes += (n == candidate) ? 1 : -1;
         }
 
         return candidate;
