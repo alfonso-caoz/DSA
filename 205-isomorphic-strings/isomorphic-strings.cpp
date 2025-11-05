3d562f38f2b258d309696336eb2d2165291bb2f3
Alfonso Castillo Orozco <38094003+alfonso441@users.noreply.github.com>
2025-11-04 20:04:13 -0600

    Time: 4 ms (24.09%) | Memory: 9.4 MB (27.98%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     // Time Complexity O(n) [n = s.length() = t.length()]. 
     // Space Complexity O(1) [Any valid ASCII character, which is a finite amount of elements].
+    // 128 standard ASCII characters. 256 extended ASCII characters. 2 Hash maps implemented: Constant Space.
+    // Considering constraints, to not validate if s and t are the same length, or any of them is empty.
+    // Another approach: 
+    // 1 Hash map, but 2 for loop required. One to iterate s, another to check if s[i] (keys) exists in all Values (t).: s "bd" t: "bb",
+    // count() and find() operates just over keys, not values of a hash map.
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> maps;
         unordered_map<char, char> mapt;
