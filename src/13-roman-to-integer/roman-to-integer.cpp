7155ea718957604527b700deb65389f8914c77d0
Alfonso Castillo Orozco <38094003+alfonso-caoz@users.noreply.github.com>
2026-05-09 21:22:34 -0600

    Time: 11 ms (27.83%) | Memory: 13.3 MB (39.16%) - LeetSync

--- src/13-roman-to-integer/roman-to-integer.cpp
+++ src/13-roman-to-integer/roman-to-integer.cpp
@@ -1,15 +1,18 @@
+#include <string>
+#include <unordered_map>
+
 class Solution {
 public:
-    int romanToInt(string s) {
+    int romanToInt(std::string s) {
 
         // Hash table
-        unordered_map<char, int> hash_table = { {'I', 1}, 
-                                                {'V', 5}, 
-                                                {'X', 10}, 
-                                                {'L', 50}, 
-                                                {'C', 100}, 
-                                                {'D', 500}, 
-                                                {'M', 1000} };
+        std::unordered_map<char, int> hash_table = { {'I', 1}, 
+                                                     {'V', 5}, 
+                                                     {'X', 10}, 
+                                                     {'L', 50}, 
+                                                     {'C', 100}, 
+                                                     {'D', 500}, 
+                                                     {'M', 1000} };
 
         int result = 0; // Result initialized
 
